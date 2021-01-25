e54b0c32e51ab535448daf9c9cf621cfd8a5e8c9
TimStolp <tim2stolp@gmail.com>
2021-01-25 11:53:07 +0100

    Improve seeding.

--- main.py
+++ main.py
@@ -70,8 +70,14 @@ if __name__ == '__main__':
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
 
     print(args)
-    torch.manual_seed(args.seed)
     np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(args.seed)
+        torch.cuda.manual_seed_all(args.seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    os.environ["PYTHONHASHSEED"] = str(args.seed)
 
     st = time.time()
 
