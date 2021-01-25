71ad7bff7547c35a7d967de904533f7037a73830
TimStolp <tim2stolp@gmail.com>
2021-01-25 21:17:58 +0100

    Save energy values

--- plot_energy_values.py
+++ plot_energy_values.py
@@ -6,11 +6,9 @@ energy_values = []
 for directory in os.listdir('experiments'):
     npz = np.load(os.path.join('experiments', directory, 'log', 'data', '1.npz'))
     # 'log_normalizer', 'neg_log_det', 'neg_trace', 'loss', 'perf'
-    print('loss:', -npz['loss'][-1])
     energy_values.append(-npz['loss'][-1])
-    print()
 
-print(np.mean(energy_values))
+np.save(os.path.join('results', 'energy_values.npy'), np.array(energy_values))
 plt.plot(energy_values, '-.', color='gold', label=f'iFlow:{round(np.mean(energy_values), 4)}')
 plt.legend()
 plt.xlabel('Seed Number')
